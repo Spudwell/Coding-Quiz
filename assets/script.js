67f5155c425d085a22baf1389d30c234349cd713
Spudwell <132273558+Spudwell@users.noreply.github.com>
2023-06-05 16:23:46 -0400

    added images folder and started pseudo code

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//need atleast 5 questions
+//click event on main button as well as a set interval button
+// go back to 1:28 in API day 1 to continue to pseudo code.
+
+var secondsLeft = 75
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+}
